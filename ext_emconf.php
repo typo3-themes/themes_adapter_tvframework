f768077a59167baa18d55456d950bfe2d1f01b58
Kay Strobach <github@kay-strobach.de>
2013-04-30 11:20:10 +0200

    [TASK] Update T3JQuery

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 			'php' => '5.2.0-0.0.0',
 			'typo3' => '4.5.0-6.0.99',
 			'themes' => '0.0.0-1.99.0',
-			't3jquery' => '2.1.2-2.6.1',
+			't3jquery' => '2.1.2-2.6.3',
 		),
 		'conflicts' => array(
 		),
