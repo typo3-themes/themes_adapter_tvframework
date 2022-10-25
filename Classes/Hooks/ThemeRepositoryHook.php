f25cc63b3715452fba7bd7ad7afff57953617d58
Kay Strobach <github@kay-strobach.de>
2022-10-25 14:12:18 +0200

    [FEATURE] use packagemanager to find themes

--- Classes/Hooks/ThemeRepositoryHook.php
+++ Classes/Hooks/ThemeRepositoryHook.php
@@ -3,18 +3,20 @@
 namespace KayStrobach\Themes\Adapter\TvFramework\Hooks;
 
 use KayStrobach\Themes\Adapter\TvFramework\Domain\Model\Theme;
+use TYPO3\CMS\Core\Package\PackageManager;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class ThemeRepositoryHook
 {
     function init(&$params, $pObj)
     {
-        if (array_key_exists('templavoila_framework',
-                $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']) && array_key_exists('skins',
-                $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila_framework'])) {
-            foreach ($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['templavoila_framework']['skins'] as $skin) {
-                if (trim($skin) !== '') {
-                    $pObj->add(new Theme($skin));
-                }
+        $extensions = ExtensionManagementUtility::getLoadedExtensionListArray();
+        $packageManager = GeneralUtility::makeInstance(PackageManager::class);
+        foreach ($extensions as $extension) {
+            $path = $packageManager->getPackage($extension)->getPackagePath();
+            if (is_dir($path . '/typoscript/')) {
+                $pObj->add(new Theme($extension));
             }
         }
     }
